21e5c39ebe076b90a73b65179b29e99ac8c7c013
Sam Hocevar <sam@hocevar.net>
2016-11-05 19:56:22 +0100

    Style & English export.

--- Makefile
+++ Makefile
@@ -1,10 +1,25 @@
 
 HTML = s.html jo.html jy.html
 
-all: $(HTML)
+all: $(HTML) extra
 
 clean:
 	rm -f $(HTML)
+
+extra:
+	cat s.html | sed \
+	  -e 's/beige/beige/g' \
+	  -e 's/blanche/white/g' \
+	  -e 's/bleue/blue/g' \
+	  -e 's/jaune/yellow/g' \
+	  -e 's/noire/black/g' \
+	  -e 's/orange/orange/g' \
+	  -e 's/rose/pink/g' \
+	  -e 's/rouge/red/g' \
+	  -e 's/saumon/salmon/g' \
+	  -e 's/verte/green/g' \
+	  -e 's/violette/purple/g' \
+	> ../climbing.html
 
 %.html: %.txt
 	./misc/conv.py < $^ > $@
--- misc/conv.py
+++ misc/conv.py
@@ -7,8 +7,14 @@ header = r"""
 <html>
 <head>
 <style>
+body { font-family: sans-serif; }
 table { border-collapse: collapse; }
-table, tr, td, th { border: 1px solid #205c8f; }
+table, tr, td, th {
+    border: 1px solid #205c8f;
+}
+td {
+    font-size: 0.9em;
+}
 th {
     background-color: #205C8F;
     color: white;
@@ -38,7 +44,7 @@ for l in sys.stdin.readlines():
     if m:
         voie, color, grade, result, comm = m.groups(1)
         grades[grade] = True
-        perfs[cur_day] += [(int(voie), color, grade, result)]
+        perfs[cur_day] += [(int(voie), color, grade, result, comm)]
         continue
 
     r = r'\s*([a-z0-9]* \w\w\w)\w*: *(.*)'
@@ -50,7 +56,29 @@ for l in sys.stdin.readlines():
         perfs[cur_day] = []
         continue
 
-def fmt(result):
+def gr2str(grade):
+    lut = { '4':      '#5189f1',
+            '4+':     '#3169e1',
+            '4b':     '#3169e1',
+            '5a':     '#a0eea0',
+            '5a+':    '#80de80',
+            '5b':     '#60ce60',
+            '5b+':    '#40be40',
+            '5b+/c':  '#30b630',
+            '5c':     '#20ae20',
+            '5c+':    '#009e00',
+            '5c+/6a': '#008e00',
+            '6a':     '#eeeea0',
+            '6a+':    '#dede80',
+            '6b':     '#cece60',
+            '6b+':    '#bebe40',
+            '6c':     '#aeae20',
+            '6c+':    '#9e9e00', # LOL
+          }
+    c = lut[grade] if grade in lut else 'white'
+    return '<td style="background: ' + c + '">' + grade + '</td>'
+
+def res2str(result):
     if result == 'OK':
         return '<span style="color:#30aa30">✔</span>'
     else:
@@ -65,12 +93,12 @@ for d in days:
 print('</tr>')
 
 for g in reversed(sorted(grades)):
-    print('<tr><td>' + g + '</td>')
+    print('<tr>' + gr2str(g))
     for d in days:
         s = '<td>'
-        for voie, color, grade, result in perfs[d]:
+        for voie, color, grade, result, comm in perfs[d]:
             if grade == g:
-                s += fmt(result)
+                s += res2str(result)
         s += '</td>'
         print(s)
     print('</tr>')
@@ -78,20 +106,28 @@ print('</table>')
 
 print('<p></p>')
 
-print('<table><tr><th colspan="2">Location</th><th>Grade</th><th></th>')
+print('<table><tr><th>Route</th><th>Grade</th><th></th><th>Comments</th>')
 aggregated = {}
+comments = {}
 for d in days:
-    for voie, color, grade, result in perfs[d]:
+    for voie, color, grade, result, comm in perfs[d]:
         key = (voie, color, grade)
-        if key in aggregated:
-            aggregated[key] += fmt(result)
-        else:
-            aggregated[key] = fmt(result)
+        aggregated[key] = ''
+        comments[key] = ''
+for d in days:
+    for voie, color, grade, result, comm in perfs[d]:
+        key = (voie, color, grade)
+        aggregated[key] += res2str(result)
+        if comm:
+            if comments[key]:
+                comments[key] += ' — '
+            comments[key] += d + ': ' + comm
 for g in reversed(sorted(grades)):
     for key, val in sorted(aggregated.items()):
         voie, color, grade = key
-        if grade == g:
-            print('<tr><td>' + str(voie) + '</td><td>' + color + '</td><td>' + g + '</td><td>' + val + '</td></tr')
+        if grade != g:
+            continue
+        print('<tr><td>' + str(voie) + ' ' + color + '</td>' + gr2str(g) + '<td>' + val + '</td><td>' + comments[key] + '</td></tr>')
 
 print('</table>')
 
